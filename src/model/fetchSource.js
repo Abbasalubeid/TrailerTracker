03ef6851488d614e59acd746e72c245073aeab96
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-11 14:44:11 +0200

    Popular movies instead of discover

--- src/model/fetchSource.js
+++ src/model/fetchSource.js
@@ -13,7 +13,7 @@ function transformACB(object) {
 }
 
 function discoverMovies(){
-    return fetch(BASE_URL + "discover/movie?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(BASE_URL + "movie/popular?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
 }
 
 export{discoverMovies}
