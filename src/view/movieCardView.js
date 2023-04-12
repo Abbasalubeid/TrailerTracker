4be15fe327db8fc32d085fa5a1518a52b3ce6667
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-12 13:25:30 +0200

    Render image and description with styles

--- src/view/movieCardView.js
+++ src/view/movieCardView.js
@@ -1,15 +1,22 @@
 import React from "react";
-import {IMAGE_URL} from "../model/apiConfig.js"
+import { IMAGE_URL } from "../model/apiConfig.js";
+import "../styles/movieCard.css";
 
 export default function MovieCardView(props) {
-    return (
-        <div className="movie-card">
-          {props.movie ? <img src = {`${IMAGE_URL}${props.movie.poster_path}`} alt={props.title}/> : null}
-          <div className="movie-info">
-            <h3>{props.movie.title}</h3>
-            <p>{props.movie.releaseDate}</p>
-            <p>{props.movie.overview}</p>
-          </div>
-        </div>
-      );
+  return (
+    <div className="movie-card">
+      {props.movie ? (
+        <img
+          className="movie-image"
+          src={`${IMAGE_URL}${props.movie.poster_path}`}
+          alt={props.title}
+        />
+      ) : null}
+      <div className="movie-info">
+        <h3>{props.movie.title}</h3>
+        <p>{props.movie.releaseDate}</p>
+        <p>{props.movie.overview}</p>
+      </div>
+    </div>
+  );
 }
