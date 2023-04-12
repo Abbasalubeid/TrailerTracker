9bb8e3024ea23e03ebcd217536e913371d5e20cd
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-12 15:36:18 +0200

    Slider first try

--- src/presenter/movieCardPresenter.js
+++ src/presenter/movieCardPresenter.js
@@ -1,10 +1,30 @@
 import React from "react";
 import {discoverMovies} from "../model/fetchSource.js"
 import MovieCardView from "../view/movieCardView.js";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 export default function MovieCardPresenter(){
     const [popularMovies, setPopularMovies] = React.useState([]);
 
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 10
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
 
     function mountACB(){
         discoverMovies().then((movies) => setPopularMovies(movies));
@@ -13,7 +33,7 @@ export default function MovieCardPresenter(){
     React.useEffect(mountACB, []);
 
     return (
-        <div className="movie-card-container">
+        <Carousel responsive={responsive} className="movie-card-container">
           {popularMovies &&
             popularMovies.map((movie) => (
               <MovieCardView
@@ -21,6 +41,6 @@ export default function MovieCardPresenter(){
                 movie = {movie}
               />
             ))}
-        </div>
+        </Carousel>
       );
 }
