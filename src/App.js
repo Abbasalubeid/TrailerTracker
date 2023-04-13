83c61cabcfe811ff29b74b272eb1ea1c41c2a71b
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-13 15:52:49 +0200

    Router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './styles/App.css';
 import HomepagePresenter from './presenter/homepagePresenter';
+import { Routes, Route,Navigate } from "react-router-dom";
+import React from 'react';
+import Sidebar from './view/sidebar';
 
 function App() {
+
+  const [navVisible, showNavbar] = React.useState(false);
+
+
   return (
-    <div className='App'>
-    <HomepagePresenter/>
-    </div>
+    <div>
+    <Sidebar visible={ navVisible } show={ showNavbar } />
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path='/home' element={
+        <div className={!navVisible ? "page" : "page page-with-navbar"}>
+          <HomepagePresenter>  
+          </HomepagePresenter>
+        </div>
+      } />
+    </Routes>
+  </div>
   );
 }
 
