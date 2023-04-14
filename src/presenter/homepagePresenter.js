01250cb3aa68b97b8ffb659345b27ca23664388d
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-14 22:29:53 +0200

    Responsive posters

--- src/presenter/homepagePresenter.js
+++ src/presenter/homepagePresenter.js
@@ -9,25 +9,25 @@ export default function HomepagePresenter(){
     const [popularMovies, setPopularMovies] = React.useState([]);
     const [trendMovies, setTrendMovies] = React.useState([]);
 
-    const responsive = {
+    const responsiveCards = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 1700 },
             items: 6
           },
         LargerDesktop: {
-            breakpoint: { max: 1700, min: 1100},
+            breakpoint: { max: 1700, min: 1300},
             items: 5
           },
         desktop: {
-          breakpoint: { max: 1100, min: 850},
-          items: 3
+          breakpoint: { max: 1300, min: 1080},
+          items: 4
         },
         largerTablet: {
-          breakpoint: { max: 850, min: 600 },
+          breakpoint: { max: 1080, min: 800 },
           items: 3
         },
         tablet: {
-          breakpoint: { max: 600, min: 350 },
+          breakpoint: { max: 800, min: 350 },
           items: 2
         },
         mobile: {
@@ -36,18 +36,49 @@ export default function HomepagePresenter(){
         }
       };
 
+      const responsivePosters = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 1700 },
+            items: 4
+          },
+        LargerDesktop: {
+            breakpoint: { max: 1700, min: 950},
+            items: 3
+          },
+        tablet: {
+            breakpoint: { max: 950, min: 630},
+            items: 2
+          },
+        mobile: {
+          breakpoint: { max: 630, min: 0 },
+          items: 1
+        },
+      };
+
     function mountACB(){
         discoverMovies().then((movies) => setPopularMovies(movies));
-        trendingMovies().then((movies) => console.log(movies));
+        trendingMovies().then((movies) => setTrendMovies(movies));
     }
 
     React.useEffect(mountACB, []);
 
     return (
         <div>
-          <MoviePoster></MoviePoster>
           <Carousel 
-        responsive={responsive} 
+        responsive = {responsivePosters} 
+        showDots = {true}
+        keyBoardControl = {true}
+        >
+          {trendMovies &&
+            trendMovies.map((movie) => (
+              <MoviePoster
+                key = {movie.id}
+                movie = {movie}
+              />
+            ))}
+        </Carousel>
+          <Carousel 
+        responsive={responsiveCards} 
         showDots={true}
         keyBoardControl={true}
         >
