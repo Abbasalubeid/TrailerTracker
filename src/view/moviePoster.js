296a044a3f41cc222eb068b839ce84da0cfd7dd4
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-14 16:11:19 +0200

    Render posters

--- src/view/moviePoster.js
+++ src/view/moviePoster.js
@@ -4,20 +4,16 @@ import "../styles/moviePoster.css";
 
 export default function MoviePoster(props) {
 
-    const poster = "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg"
-    const title = "The Super Mario Bros. Movie"
-    const date = "2023-04-05"
-
     return (
         <div className="movie-card movie-poster">
             <img
               className="movie-image"
-              src={`${IMAGE_URL}${poster}`}
-              alt={props.title}
+              src={`${IMAGE_URL}${props.movie.backdrop_path}`}
+              alt={props.movie.title}
             />
         <div className="poster-info">
-        <h3>{title}</h3>
-        <p>{Number(date.split("-")[0])}</p>
+        <h3>{props.movie.title}</h3>
+        <p>{Number(props.movie.release_date.split("-")[0])}</p>
       </div>
     </div>
       );
