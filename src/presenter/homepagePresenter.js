7c05e9051a052e6421453639932a2ec8a3e7606a
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-14 23:37:37 +0200

    Code cleanup

--- src/presenter/homepagePresenter.js
+++ src/presenter/homepagePresenter.js
@@ -2,58 +2,30 @@ import React from "react";
 import {discoverMovies, trendingMovies} from "../model/fetchSource.js"
 import MovieCard from "../view/movieCard.js";
 import MoviePoster from "../view/moviePoster.js";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function HomepagePresenter(){
     const [popularMovies, setPopularMovies] = React.useState([]);
     const [trendMovies, setTrendMovies] = React.useState([]);
 
     const responsiveCards = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 1700 },
-            items: 6
-          },
-        LargerDesktop: {
-            breakpoint: { max: 1700, min: 1300},
-            items: 5
-          },
-        desktop: {
-          breakpoint: { max: 1300, min: 1080},
-          items: 4
-        },
-        largerTablet: {
-          breakpoint: { max: 1080, min: 800 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 800, min: 350 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 350, min: 0 },
-          items: 1
-        }
-      };
-
-      const responsivePosters = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 1700 },
-            items: 4
-          },
-        LargerDesktop: {
-            breakpoint: { max: 1700, min: 950},
-            items: 3
-          },
-        tablet: {
-            breakpoint: { max: 950, min: 630},
-            items: 2
-          },
-        mobile: {
-          breakpoint: { max: 630, min: 0 },
-          items: 1
-        },
-      };
+      superLargeDesktop: { breakpoint: { max: 4000, min: 1700 }, items: 9 },
+      LargerDesktop: { breakpoint: { max: 1700, min: 1300 }, items: 7 },
+      desktop: { breakpoint: { max: 1300, min: 1080 }, items: 6 },
+      largerTablet: { breakpoint: { max: 1080, min: 805 }, items: 5 },
+      tablet2: { breakpoint: { max: 805, min: 660 }, items: 4 },
+      tablet1: { breakpoint: { max: 660, min: 510 }, items: 3 },
+      mobile2: { breakpoint: { max: 380, min: 0 }, items: 2 },
+      mobile1: { breakpoint: { max: 310, min: 0 }, items: 1 },
+    };
+  
+    const responsivePosters = {
+      superLargeDesktop: { breakpoint: { max: 4000, min: 1700 }, items: 4 },
+      LargerDesktop: { breakpoint: { max: 1700, min: 950 }, items: 3 },
+      tablet: { breakpoint: { max: 950, min: 630 }, items: 2 },
+      mobile: { breakpoint: { max: 630, min: 0 }, items: 1 },
+    };
 
     function mountACB(){
         discoverMovies().then((movies) => setPopularMovies(movies));
@@ -63,11 +35,12 @@ export default function HomepagePresenter(){
     React.useEffect(mountACB, []);
 
     return (
-        <div>
+        <>
+          <h1 className="title">Trending</h1>
           <Carousel 
         responsive = {responsivePosters} 
-        showDots = {true}
         keyBoardControl = {true}
+        itemClass="carousel-item-padding-40-px"
         >
           {trendMovies &&
             trendMovies.map((movie) => (
@@ -77,10 +50,11 @@ export default function HomepagePresenter(){
               />
             ))}
         </Carousel>
-          <Carousel 
+        <h1 className="title">Discover</h1>    
+        <Carousel 
         responsive={responsiveCards} 
-        showDots={true}
         keyBoardControl={true}
+        itemClass="carousel-item-padding-40-px"
         >
           {popularMovies &&
             popularMovies.map((movie) => (
@@ -90,7 +64,7 @@ export default function HomepagePresenter(){
               />
             ))}
         </Carousel>
-        </div>
+        </>
 
       );
 }
