9ed8b133e4e635f74c2457c6d6ffc64dafeb2ca7
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-15 19:13:25 +0200

    Parent element with for CSS positioning for hover bugg

--- src/view/movieCard.js
+++ src/view/movieCard.js
@@ -6,16 +6,18 @@ export default function MovieCard(props) {
   return (
     <div className={props.carousel ? "movie-card-carousel" : ""}>
       {props.movie ? (
-        <img
-          className={props.carousel ? "movie-image-carousel" : "movie-image"}
-          src={`${IMAGE_URL}${props.movie.poster_path}`}
-          alt={props.title}
-        />
+        <div className="movie-image-container">
+          <img
+            className={props.carousel ? "movie-image-carousel" : "movie-image"}
+            src={`${IMAGE_URL}${props.movie.poster_path}`}
+            alt={props.title}
+          />
+          <div className="movie-info">
+            <h3>{props.movie.title}</h3>
+            <p>{Number(props.movie.release_date.split("-")[0])}</p>
+          </div>
+        </div>
       ) : null}
-      <div className="movie-info">
-        <h3>{props.movie.title}</h3>
-        <p>{Number(props.movie.release_date.split("-")[0])}</p>
-      </div>
     </div>
   );
 }
