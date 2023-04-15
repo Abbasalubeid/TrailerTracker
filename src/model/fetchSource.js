5dc4c2456097b219fc1d92c3094e19a6ca18fd83
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-15 14:25:03 +0200

    Typo

--- src/model/fetchSource.js
+++ src/model/fetchSource.js
@@ -25,7 +25,7 @@ function upcomingMovies(){
 }
 
 function discoverMovies(){
-    return fetch(BASE_URL + "movie/discover?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(BASE_URL + "discover/movie?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
 }
 
 export{topRatedMovies, trendingMovies, upcomingMovies, discoverMovies}
