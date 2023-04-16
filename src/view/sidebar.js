9831d614839a375bde140814d7ded0101d19b956
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-16 18:41:56 +0200

    Canceled the seperated sidebar for big screens

--- src/view/sidebar.js
+++ src/view/sidebar.js
@@ -4,8 +4,6 @@ import "../styles/sidebar.css";
 import {
     FaHome,
     FaCompass,
-    FaAngleRight,
-	FaAngleLeft,
     FaBars 
 } from 'react-icons/fa';
 
@@ -14,26 +12,16 @@ export default function Sidebar(props) {
 
   return (
     <>
-      <div className="mobile-nav">
+      <div className="nav-top">
         <button
-          className="mobile-nav-btn"
+          className="nav-btn"
           onClick={() => props.show(!props.visible)}
         >
           <FaBars size={24} />
         </button>
       </div>
+      
       <nav className={!props.visible ? "sidebar" : ""}>
-        <button
-          type="button"
-          className="nav-btn"
-          onClick={() => props.show(!props.visible)}
-        >
-          {!props.visible ? (
-            <FaAngleRight size={30} />
-          ) : (
-            <FaAngleLeft size={30} />
-          )}
-        </button>
         <div>
           <NavLink to="/home" className="nav-link">
             <FaHome size={20} />
