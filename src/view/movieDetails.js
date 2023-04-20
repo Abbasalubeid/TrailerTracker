1de4ef2fd93f96b61c4049682552b27970dd4a87
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-20 23:37:53 +0200

    Show voting with one decimal

--- src/view/movieDetails.js
+++ src/view/movieDetails.js
@@ -3,14 +3,9 @@ import { FaStar } from "react-icons/fa";
 import "../styles/movieDetails.css"
 export default function MovieDetails(props){
 
-  const backgroundStyle = {
-    backgroundImage: `url(${IMAGE_URL}${props.movie.backdrop_path})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
 
   return (
-    <div className="movie-details" style={backgroundStyle}>
+    <div className="movie-details">
       <img
         className="movie-detail-poster"
         src={`${IMAGE_URL}${props.movie.poster_path}`}
@@ -21,7 +16,7 @@ export default function MovieDetails(props){
         <p className="movie-detail-overview">{props.movie.overview}</p>
         <div className="movie-rating">
           <FaStar className="star-icon" />
-          <p className="rating-value">{props.movie.vote_average}</p>
+          <p className="rating-value">{props.movie.vote_average.toFixed(1)}</p>
         </div>
         <p className="release-date">
           Release Date: {new Date(props.movie.release_date).toLocaleDateString()}
