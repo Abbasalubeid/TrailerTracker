b8bf2a7533f276cf4bbaad46a81b4b43ffdd51be
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-23 14:48:54 +0200

    Fetch trailers from API

--- src/model/fetchSource.js
+++ src/model/fetchSource.js
@@ -28,4 +28,8 @@ function discoverMovies(){
     return fetch(BASE_URL + "discover/movie?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
 }
 
-export{topRatedMovies, trendingMovies, upcomingMovies, discoverMovies}
+function getVideo(id){
+    return fetch(BASE_URL + `movie/${id}/videos?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
+}
+
+export{topRatedMovies, trendingMovies, upcomingMovies, discoverMovies, getVideo}
