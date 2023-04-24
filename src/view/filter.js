25fe1e5b0907309f680cfcd797f49a080b0265d4
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-24 22:49:53 +0200

    Eliminate redudant code by array rendering

--- src/view/filter.js
+++ src/view/filter.js
@@ -9,14 +9,16 @@ export default function Filter(props) {
 
     return (
         <div className="filter-container">
-            <button value="28" onClick={genreChangedACB}>Action</button>
-            <button value="878" onClick={genreChangedACB}>Science fiction</button>
-            <button value="12" onClick={genreChangedACB}>Adventure</button>
-            <button value="14" onClick={genreChangedACB}>Fantasy</button>
-            <button value="10752" onClick={genreChangedACB}>War</button>
-            <button value="16" onClick={genreChangedACB}>Animation</button>
-            <button value="18" onClick={genreChangedACB}>Drama</button>
-            <button value="27" onClick={genreChangedACB}>Horror</button>
+            {props.genres.map(genre => (
+                <button
+                    key={genre.id}
+                    className={props.activeGenre === genre.id ? "active" : ""}
+                    value={genre.id}
+                    onClick={genreChangedACB}
+                >
+                    {genre.name}
+                </button>
+            ))}
         </div>
     );
 }
