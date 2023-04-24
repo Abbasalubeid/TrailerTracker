0497c00dcf82ce5d9198a06f6b66ba6a454b3223
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-24 21:42:26 +0200

    Filters first try

--- src/presenter/discoverPresenter.js
+++ src/presenter/discoverPresenter.js
@@ -1,15 +1,21 @@
 import React from "react";
 import {discoverMovies} from "../model/fetchSource.js"
 import MovieCard from "../view/movieCard.js";
+import SearchView from "../view/searchView.js"
 import "../styles/movieCard.css"
+import Filter from "../view/filter.js";
 
 export default function DiscoverPresenter(props){
     const [movies, setMovies] = React.useState([]);
+    const [activeGenre, setActiveGenre] = React.useState(0);
 
     function mountACB(){
-        discoverMovies().then((movies) => setMovies(movies));
-    }
-
+      discoverMovies().then((movies) => {
+          setMovies(movies);
+      });
+  }
+  
+  console.log(activeGenre);
     function setCurrentMovieACB(movie){
         props.setCurrentMovie(movie);
       }
@@ -17,12 +23,22 @@ export default function DiscoverPresenter(props){
     React.useEffect(mountACB, []);
 
     return (
-    <div className="movie-card">
-        {movies &&
-            movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} 
-            onMovieChoice = {setCurrentMovieACB}/>
-          ))}
-    </div>
-    );
+        <>
+          <SearchView />
+          <Filter 
+          allMovies={movies}
+          activeGenre={activeGenre}
+          setActiveGenre={setActiveGenre}/>
+          <div className="movie-card">
+            {movies &&
+              movies.map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  movie={movie}
+                  onMovieChoice={setCurrentMovieACB}
+                />
+              ))}
+          </div>
+        </>
+      );
 }
