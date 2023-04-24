36d0e46913e5df2769c9e5e460afb47f7f2673ec
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-24 23:48:14 +0200

    String interpolation convention

--- src/model/fetchSource.js
+++ src/model/fetchSource.js
@@ -17,27 +17,27 @@ function transfromDetailsACB(object) {
 }
 
 function topRatedMovies(){
-    return fetch(BASE_URL + "movie/top_rated?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(`${BASE_URL}movie/top_rated?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
 function trendingMovies(){
-    return fetch(BASE_URL + "trending/movie/day?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(`${BASE_URL}trending/movie/day?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
 function upcomingMovies(){
-    return fetch(BASE_URL + "movie/upcoming?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(`${BASE_URL}movie/upcoming?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
 function discoverMovies(){
-    return fetch(BASE_URL + "discover/movie?api_key=" + API_KEY).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(`${BASE_URL}discover/movie?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
 function getVideo(id){
-    return fetch(BASE_URL + `movie/${id}/videos?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
+    return fetch(`${BASE_URL}movie/${id}/videos?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
 function getMovieDetails(id) {
-    return fetch(BASE_URL + `movie/${id}?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transfromDetailsACB);
+    return fetch(`${BASE_URL}movie/${id}?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transfromDetailsACB);
   }
 
 export{topRatedMovies, trendingMovies, upcomingMovies, discoverMovies, getVideo, getMovieDetails}
