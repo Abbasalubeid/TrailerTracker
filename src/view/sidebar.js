52bf9082b86aca2f37d9122b094cef9ce997e461
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-28 20:09:49 +0200

    Conditional highlighting

--- src/view/sidebar.js
+++ src/view/sidebar.js
@@ -1,36 +1,43 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
-import "../styles/sidebar.css";
-import {
-    FaHome,
-    FaCompass,
-    FaBars,
-    FaTimes
-} from 'react-icons/fa';
+import { NavLink, useLocation } from 'react-router-dom';
+import '../styles/sidebar.css';
+import { FaHome, FaCompass, FaBars, FaTimes } from 'react-icons/fa';
 
 export default function Sidebar(props) {
   const ICON_SIZE = 20;
+  const location = useLocation();
 
   return (
     <>
       <div className="nav-top">
-        <button
-          className="nav-btn"
-          onClick={() => props.show(!props.visible)}
-        >
+        <button className="nav-btn" onClick={() => props.show(!props.visible)}>
           {props.visible ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
       </div>
-      
-      <nav className={!props.visible ? "sidebar" : ""}>
+
+      <nav className={!props.visible ? 'sidebar' : ''}>
         <div>
-          <NavLink to="/home" className="nav-link">
-            <FaHome size={20} />
-            <span>Home</span>
+          <NavLink
+            to="/home"
+            className={location.pathname === "/home" ? "nav-link active-nav" : "nav-link"}
+          >
+            <FaHome
+              size={20}
+            />
+            <span >
+              Home
+            </span>
           </NavLink>
-          <NavLink to="/discover" className="nav-link">
-            <FaCompass size={ICON_SIZE} />
-            <span>Discover</span>
+          <NavLink
+            to="/discover"
+            className={location.pathname === "/discover" ? "nav-link active-nav" : "nav-link"}
+          >
+            <FaCompass
+              size={ICON_SIZE}
+            />
+            <span>
+              Discover
+            </span>
           </NavLink>
         </div>
       </nav>
