5d1a968cf340bd28e6238367822f2ca6e126ea0c
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-29 18:37:00 +0200

    Rerender when new movie is chosen

--- src/view/loading.js
+++ src/view/loading.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import "../styles/loading.css";
 
-export default function Loading({ error, children }) {
-    const [loading, setLoading] = React.useState(true);
+export default function Loading({ key, error, children }) {
+  const [loading, setLoading] = React.useState(true);
 
-    React.useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 500);
+  React.useEffect(() => {
+    console.log(loading);
+    console.log(key);
+      const timer = setTimeout(() => {
+          setLoading(false);
+      }, 500);
 
-        return () => clearTimeout(timer); // Clean up the timer when the component unmounts
-    }, []);
+      return () => clearTimeout(timer); // Clean up the timer when the component unmounts
+  }, [key]);
 
-    if (error && !loading) {
-        return <div className="error-container">{error.message}</div>
-    }
+  if (error && !loading) {
+    return <div className="error-container">{error.message}</div>
+}
 
     if (children && !loading) {
         return children;
