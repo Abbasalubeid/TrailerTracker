30112069c2bd4f502cf7fd95fad867e454019074
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-04-29 20:28:06 +0200

    Shake animation when input is invalid

--- src/view/searchBar.js
+++ src/view/searchBar.js
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "../styles/searchBar.css";
 
 export default function SearchBar(props) {
-    
+
   function onFormChange(event) {
     event.preventDefault();
     props.userSearched(event.target.value);
   }
 
-  function onFormSubmit(event){
+  function onFormSubmit(event) {
     event.preventDefault();
-    props.userSearched(event.target.searchInput.value);
+    props.userSearched(event.target.searchInput.value)
   }
 
   return (
-    <form 
-    onChange={onFormChange}
-    onSubmit={onFormSubmit}>
-      <div className="search-bar">
-        <div className="search-icon">
+    <form onChange={onFormChange} onSubmit={onFormSubmit}>
+      <div className={`search-bar ${props.hasError ? "error" : ""}`}>
+        <div
+          className={`search-icon ${
+            props.hasError ? "error shake-animation" : ""
+          }`}
+        >
           <FaSearch />
         </div>
-        <input type="text" placeholder="Search with titles" name="searchInput" />
+        <input
+          className={`search-input ${
+            props.hasError ? "error shake-animation" : ""
+          }`}
+          type="text"
+          placeholder="Search with titles"
+          name="searchInput"
+        />
       </div>
     </form>
   );
