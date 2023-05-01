4200b52f030acc4fc4261b677c6f38d23e2dbf5a
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-05-01 21:22:52 +0200

    Loading skeleton for trailers since they can be slow to be ready

--- src/view/trailerCard.js
+++ src/view/trailerCard.js
@@ -1,16 +1,41 @@
+import React from "react";
 import "../styles/movieDetails.css";
 import YouTube from "react-youtube";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function TrailerCard(props) {
+  const [isReady, setIsReady] = React.useState(false);
 
   function handleVideoErrorACB() {
     props.onVideoError(props.trailer.id);
   }
 
+  function handleOnReady() {
+    setIsReady(true);
+  }
+
   return (
     <div className="trailer-card">
       <div className="trailer-vid">
-        <YouTube videoId={props.trailer?.key} onError={handleVideoErrorACB} />
+        {!isReady && (
+          <SkeletonTheme
+          baseColor="rgba(23, 23, 23, 1)"
+            highlightColor="#ffffff"
+          >
+            <Skeleton
+              width="100%"
+              height="0"
+              style={{ paddingTop: "56.25%", borderRadius: "25px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)", transition: "all 0.3s ease-in-out" }}
+            />
+          </SkeletonTheme>
+        )}
+        <YouTube
+          videoId={props.trailer?.key}
+          onError={handleVideoErrorACB}
+          onReady={handleOnReady}
+          style={{ display: isReady ? "block" : "none", borderRadius: "25px" }}
+        />
       </div>
     </div>
   );
