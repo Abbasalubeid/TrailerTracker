4290c037c0098f6da75aef2a7235ae62f4bc9be4
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-05-06 17:39:23 +0200

    Eliminate irrelevant result from API

--- src/model/fetchSource.js
+++ src/model/fetchSource.js
@@ -28,11 +28,12 @@ function upcomingMovies(){
     return fetch(`${BASE_URL}movie/upcoming?api_key=${API_KEY}`).then(treatHTTPResponseACB).then(transformACB);
 }
 
-function discoverMovies(page, genre = 0, sortBy = "") {
+function discoverMovies(page, genre = 0, sortBy = "", minVoteCount = 500) {
   const sortByParam = sortBy ? `&sort_by=${sortBy}` : "";
+  const minVoteCountParam = `&vote_count.gte=${minVoteCount}`;
   const url = genre === 0
-    ? `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${page}${sortByParam}`
-    : `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${page}&with_genres=${genre}${sortByParam}`;
+    ? `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${page}${sortByParam}${minVoteCountParam}`
+    : `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${page}&with_genres=${genre}${sortByParam}${minVoteCountParam}`;
   
     return fetch(url)
       .then(treatHTTPResponseACB)
