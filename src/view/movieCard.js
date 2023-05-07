ea0719a9278cb1cc8d1f697f9de786c631e5a3b1
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-05-07 22:36:08 +0200

    Removed unused code

--- src/view/movieCard.js
+++ src/view/movieCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { IMAGE_URL } from "../model/apiConfig.js";
 import "../styles/movieCard.css";
